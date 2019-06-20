85dd12a823beeed39d22537b1e09967e5e1688ab
Vandana Sharma <vandana.sharma2@loblaw.ca>
2019-06-20 12:20:55 -0400

    added error page for 404 route

--- src/RoutesDetail.jsx
+++ src/RoutesDetail.jsx
@@ -1,30 +1,52 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import UsersLayoutContainer from './containers/layouts/UsersLayoutContainer.js';
 import UsersListContainer from './containers/userList/UsersListContainer.js';
 import UserDetailsContainer from './containers/userDetail/UserDetailsContainer.js';
+import ErrorPage from './components/common/ErrorPage.jsx';
 
 const RoutesDetail = () => {
     return (
       <div className="app-content">
-        <Route
-          exact
-          path="/"
-          render={(props) => (
-            <UsersLayoutContainer>
-              <UsersListContainer {...props} />
-            </UsersLayoutContainer>
-          )}
-        />
-        <Route
-          exact
-          path="/details/:userId"
-          render={(props) => (
-            <UsersLayoutContainer>
-              <UserDetailsContainer {...props} />
-            </UsersLayoutContainer>
+        <Switch>
+          <Route
+            exact
+            path="/user-list"
+            render={(props) => (
+              <UsersLayoutContainer>
+                <UsersListContainer {...props} />
+              </UsersLayoutContainer>
             )}
-        />
+            key="userListPage"
+          />
+          <Route
+            exact
+            path="/details/:userId"
+            render={(props) => (
+              <UsersLayoutContainer>
+                <UserDetailsContainer {...props} />
+              </UsersLayoutContainer>
+              )}
+            key="userDetailPage"
+          />
+          <Route
+            exact
+            path="/404"
+            component={ErrorPage}
+            key="404Route"
+          />,
+          <Redirect
+            exact
+            from="/"
+            to="/user-list"
+            key="homeNewUserRedirect"
+          />
+          <Redirect
+            from="*"
+            to="/404"
+            key="404Redirect"
+          />,
+        </Switch>  
       </div>
     );
 }
---
+++ src/components/common/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className="error-page">
+            Sorry, this page doesn't exist!
+        </div>
+    );
+}
+export default ErrorPage;
